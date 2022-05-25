62aff59df6964ce5129bf16558c00c12b66c831c
walid <walidsharar@gmail.com>
2022-05-26 00:35:26 +0200

    plot the data using seaborn

--- main.py
+++ main.py
@@ -4,6 +4,11 @@ import matplotlib.pyplot as plt
 
 
 stock = ["AAPL","XPEV"]
-yf.download(stock, start ="2010-01-01" ,end="2022-26-05" )
+stocks= yf.download(stock, start ="2010-01-01" ,end="2022-26-05" )
+data = stocks.loc[:,"close"].copy()
 
 
+
+data.plot(figsize=(17,8),fontsize=18)
+plt.style.use("seaborn")
+plt.show()
