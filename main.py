9fb2659bbd46084cfd2353e75e0980d18e210a60
walid <walidsharar@gmail.com>
2022-05-26 00:46:56 +0200

    plot the frequency

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import matplotlib.pyplot as plt
 
 
 stock = ["AAPL","XPEV"]
-stocks= yf.download(stock, start ="2010-01-01" ,end="2022-26-05" )
+stocks= yf.download(stock, start ="2020-01-01" ,end="2022-26-05" )
 data = stocks.loc[:,"close"].copy()
 
 
@@ -12,3 +12,18 @@ data = stocks.loc[:,"close"].copy()
 data.plot(figsize=(17,8),fontsize=18)
 plt.style.use("seaborn")
 plt.show()
+
+data.head()
+#  get value of first row
+data.iloc[0]
+
+
+
+
+# check the frequency
+
+google = normData.GOOGL.copy().to_frame()
+ret= google.pct_change().dropna()
+
+ret.plot(kind="hist",figsize=(12,8),bins=100)
+plot.show()
