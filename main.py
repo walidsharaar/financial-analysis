21387b1683a55e4e69fb1b6eb2887be3b5ff2726
walid <walidsharar@gmail.com>
2022-05-26 00:54:26 +0200

    Scatter plot the stock of three company

--- main.py
+++ main.py
@@ -28,3 +28,11 @@ ret= google.pct_change().dropna()
 ret.plot(kind="hist",figsize=(12,8),bins=100)
 plot.show()
 
+
+# Stock Ris Analysis
+
+sum.plot.scatter(x="std",y="mean",figsiz=(12,8), s=50, fontsize=15)
+for i in sum.index:
+    plt.annotate(i,xy=(sum.loc[i,"std"]+0.002,sum.loc[i,"mean"]+0.002),size=15)
+
+
